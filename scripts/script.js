b2a6acff479b59008276e38b26ed943fa29af73e
Caspar <145459394+WaterEend@users.noreply.github.com>
2025-12-11 18:03:04 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var menuButton = document.querySelector("header ul li:nth-of-type(5) button")
+menuButton.onclick = open;
+
+function open() {
+    var nav = document.querySelector("nav")
+}
